a035d8ffa1d77bc6329ca4b02716be72276386d9
Briana Baker <brianaclairbaker@gmail.com>
2017-09-12 22:20:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ return anotherArray;
 
 function johnLennonFacts(){
 var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var newFacts= [];
+  var loud= [];
   var i = 0;
   while (i < facts.length) {
-    newFacts.push(facts[i] + "!!!");
+    loud.push(facts[i] + "!!!");
     i++;
   }
-  return newFacts;
+  return loud;
 }
