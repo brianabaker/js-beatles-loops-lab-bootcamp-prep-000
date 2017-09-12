25f72a7a75e9a24c2a53b824e3d7e8598e893e9b
Briana Baker <brianaclairbaker@gmail.com>
2017-09-12 22:11:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ return anotherArray;
 }
 
 function johnLennonFacts(){
-  
+var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
 }
