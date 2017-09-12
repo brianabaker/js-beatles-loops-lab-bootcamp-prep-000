8e12e774cdcd239c7578a5910a0c0dc209206a39
Briana Baker <brianaclairbaker@gmail.com>
2017-09-12 22:05:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
 function theBeatlesPlay(musicians, instruments){
   var anotherArray= [];
   for (var i=0; i<anotherArray.length; i++){
-    anotherArray.push(players[i] + "plays" + instruments[i]);
+    anotherArray.push(musicians[i] + "plays" + instruments[i]);
   }
-return anotherArray
+return anotherArray;
 }
