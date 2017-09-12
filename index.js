cf8614369270dd396c3c7ff2f2a22dd9ace0b25c
Briana Baker <brianaclairbaker@gmail.com>
2017-09-12 22:02:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 function theBeatlesPlay(musicians, instruments){
   var anotherArray= [];
-  for (var i=0, i<anotherArray.length, i++){
+  for (var i=0; i<anotherArray.length; i++){
     newArray.push(players[i] + "plays" + instruments[i]);
   }
 return anotherArray
