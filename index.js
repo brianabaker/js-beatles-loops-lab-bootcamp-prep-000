a3c20b3d4a89e154e906fe4ce0b31388b92a5941
Briana Baker <brianaclairbaker@gmail.com>
2017-09-12 22:19:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function theBeatlesPlay(musicians, instruments){
 return anotherArray;
 }
 
-function johnLennonFacts(){
+function johnLennonFacts(facts){
 var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var newFacts= [];
   var i = 0;
